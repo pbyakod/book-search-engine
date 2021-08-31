0b179235c593e7003a93b13f43049d60dfa9c16c
Pranav Byakod <pranav.byakod@gmail.com>
2021-08-31 16:42:55 -0700

    deploying to Heroku

--- server/server.js
+++ server/server.js
@@ -25,6 +25,16 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/books',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+);
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
