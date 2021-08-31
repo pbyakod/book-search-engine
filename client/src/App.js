57803a820713122db552d9db040430160e7c042f
Pranav Byakod <pranav.byakod@gmail.com>
2021-08-30 20:54:00 -0700

    making an apollo provider which allows every request to work with the server

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,48 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// getting the neccesary imports from the apollo client
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context';
+
+// making the graphql http link
+const gqlHttpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+// generating middleware with a jswebtoken attached to requests
+const gqlAuthLink = setContext(( _, { headers } ) => {
+  // retrieving the auth token from localstorage and returns the headers to the graphql http link
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}`: '',
+    },
+  }
+});
+
+// executing the gqlauthlink middleware for our graphql api
+const gqlClient = new ApolloClient({
+  link: gqlAuthLink.concat(gqlHttpLink),
+  cache: new InMemoryCache()
+});
+
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    // implementing thee apolloprovider for our client
+    <ApolloProvider client={ client }>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
